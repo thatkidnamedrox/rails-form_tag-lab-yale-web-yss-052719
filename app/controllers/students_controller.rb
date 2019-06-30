62e9be947b495025a5ba423beba1b36bbb989ff5
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-30 06:29:44 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,9 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    puts params
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    puts @student
     redirect_to student_path(@student)
   end
 
